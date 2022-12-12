e0a7bc0d76f49c02bef589d1b4d5a2b2b9013c73
azertyDev <khikmatovumid@mail.ru>
2022-12-12 17:01:53 +0500

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,46 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Montserrat } from "@next/font/google";
+import Head from "next/head";
+
+const montserrat = Montserrat({
+    subsets: ["latin"],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>1Zap</title>
+                <meta name="description" content="Купи и сравни" />
+                <link rel="icon" href="/favicon/favicon.ico" />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/favicon/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon/favicon-16x16.png"
+                />
+                <meta name="theme-color" content="#ee3300" />
+            </Head>
+            <style jsx global>{`
+                html {
+                    font-family: ${montserrat.style.fontFamily};
+                }
+            `}</style>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,5 @@
+import s from './index.module.css';
+
+export const Home = () => {
+    return <div className={s.home}>Home</div>;
+};
