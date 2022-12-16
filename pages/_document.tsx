6a88c994c9e0dea4bd3e60021de03c66df100f19
Daniil <gracianodragus@gmail.com>
2022-12-16 15:55:26 +0500

    added icons

---
+++ pages/_document.tsx
@@ -0,0 +1,37 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
+
+class MyDocument extends Document {
+    static async getInitialProps(
+        ctx: DocumentContext
+    ): Promise<DocumentInitialProps> {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps };
+    }
+
+    render() {
+        return (
+            <Html lang="ru">
+                <Head>
+                    <meta charSet="utf-8" />
+                    <link
+                        rel="stylesheet"
+                        href="https://cdn.jsdelivr.net/npm/@mdi/font@6.9.96/css/materialdesignicons.min.css"
+                    />
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+                    />
+                </Head>
+
+                <body>
+                    <div id="sidebar_menu"></div>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
