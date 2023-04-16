188ad51d4e188e938f84cb6575f10da717bf3cdc
azertyDev <khikmatovumid@mail.ru>
2023-04-16 18:23:11 +0500

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
     deploy: {
         production: {
             user: 'root',
-            host: ['185.217.131.180'],
+            host: ['185.217.131.180:22'],
             ref: 'origin/main',
             repo: 'git@github.com:azertyDev/1zap.git',
             path: '/opt/1zap_front',
