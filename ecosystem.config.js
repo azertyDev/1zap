4af88caf5b500f5e096ef1c09c90c3c09a03d070
azertyDev <khikmatovumid@mail.ru>
2023-04-16 18:33:13 +0500

    Fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: '1zap_front',
+            name: '1zap',
             exec_mode: 'cluster',
             instances: '1',
             script: 'npm',
