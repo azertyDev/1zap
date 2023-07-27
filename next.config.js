78b5bb5a329826516963301a5766285b2f77c19a
Daniil <gracianodragus@gmail.com>
2023-07-27 13:07:25 +0500

    removed distDir

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const { i18n } = require('./next-i18next.config');
 const nextConfig = {
     reactStrictMode: false,
     i18n,
-    distDir: 'build',
+    // distDir: 'build',
     images: {
         domains: ['images.unsplash.com', 'img.laximo.ru', '1zap.uz'],
     },
